c5857903786389ff1de5bec8e61f110ffae09514
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-09-15 15:24:06 +0200

    Update index.js
    Contains a typo somewhere.

---
+++ index.js
@@ -0,0 +1,137 @@
+window.onload = () => {
+  // API KEY:
+  const apiKey = "563492ad6f917000010000018b655e92ca224e91ba8407a1cd0c5a56";
+
+  // EXERCISE 1:
+
+  function showImages() {
+    // FETCH IMAGES:
+    fetch("https://api.pexels.com/v1/search?query=flowers", {
+      headers: {
+        Authorization: apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then(responseBody.photos => {
+
+      console.log(responseBody.photos);
+      
+      // CONTAINER FOR IMAGES:
+      let row = document.querySelector(".row");
+
+      for(let image of responseBody.photos) {
+      let divCard = document.createElement("div");
+      divCard.className ="col-md-4";
+      
+      divCard.innerHTML = `<div class="card mb-4 shadow-sm">
+                <svg
+                  class="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns=${image.src.medium}
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c" />
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div class="card-body">
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div class="btn-group">
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>`
+
+      row.appendChild(divCard);
+      }
+    })
+    .catch((error) => console.log(error));
+    
+  }
+
+  showImages();
+
+  /* 
+fetch("https://api.pexels.com/v1/search?query=your-query", {
+  headers: {
+    Authorization: apiKey,
+  },
+});
+ <div class="col-md-4">         
+<div class="card mb-4 shadow-sm">
+                <svg
+                  class="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c" />
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div class="card-body">
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div class="btn-group">
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+              </div>
+*/
+}
+
+
+
+
