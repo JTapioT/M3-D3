32934312bd001de5d7657df478cbf36fcc19d1b5
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-09-15 16:55:15 +0200

    Update index.js
    Latest from exercises.

--- index.js
+++ index.js
@@ -31,12 +31,21 @@ window.onload = () => {
       <div class="card-body">
         <h5 class="card-title">${responseBody.photos[i].id}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-warning hide-btn">HIDE</a>
       </div>`;
-      
-
       row.appendChild(divCard);
       }
       console.log(row);
+      
+      // HIDE THE CARDS FUNCTIONALITY
+      let btnHide = document.querySelectorAll(".card");
+      console.log(btnHide);
+      btnHide.forEach((card) => {
+        card.addEventListener("click", () => {
+          card.classList.add("d-none");
+        });
+      });
+
     })
     .catch((error) => console.log(error));
     
@@ -46,59 +55,71 @@ window.onload = () => {
   primaryBtn.addEventListener("click", showImages);
 
 
-  /* 
-fetch("https://api.pexels.com/v1/search?query=your-query", {
-  headers: {
-    Authorization: apiKey,
-  },
-});
-<div class="col-md-4">         
-<div class="card mb-4 shadow-sm">
-                <svg
-                  class="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="225"
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                  role="img"
-                  aria-label="Placeholder: Thumbnail"
-                >
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595c" />
-                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                    Thumbnail
-                  </text>
-                </svg>
-                <div class="card-body">
-                  <p class="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div class="btn-group">
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        View
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Edit
-                      </button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-              </div>
-*/
+  // EXERCISE 2.
+
+  function showMoreImages() {
+    // FETCH IMAGES:
+    fetch("https://api.pexels.com/v1/search?query=forest", {
+      headers: {
+        Authorization: apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((responseBody) => {
+        console.log(responseBody.photos);
+
+        // CONTAINER FOR IMAGES:
+        let row = document.getElementById("row");
+
+        row.innerHTML = "";
+
+        for (let i = 0; i < responseBody.photos.length; i++) {
+          console.log(responseBody.photos[i].src.medium);
+
+          let divCard = document.createElement("div");
+          divCard.className = "col-md-4";
+
+          divCard.innerHTML = `<div class="card">
+      <img src=${responseBody.photos[i].src.medium} class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${responseBody.photos[i].id}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-warning hide-btn">HIDE</a>
+      </div>`;
+
+          row.appendChild(divCard);
+        }
+        console.log(row);
+        // HIDE THE CARDS FUNCTIONALITY
+        let btnHide = document.querySelectorAll(".card");
+        console.log(btnHide);
+        btnHide.forEach((card) => {
+          card.addEventListener("click", () => {
+            card.classList.add("d-none");
+          });
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  let moreImagesBtn = document.querySelector(".btn-secondary");
+  moreImagesBtn.addEventListener("click", showMoreImages);
+
+
+  // EXERCISE 3
+
+/* let btn = document.querySelector(".hide-btn");
+ const hideButtons = function(btn, callback){
+   btn.classList.add("d-none");
+  btn.forEach((card) => {
+  });
+  console.log(hideButtons);
+
+  callback();
+ }
+
+ hideButtons(btn, showMoreImages); */
+
 }
 
 
